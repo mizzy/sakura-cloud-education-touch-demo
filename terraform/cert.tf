43e2ae9c991b2491b758162faf5538ced4038f04
Gosuke Miyashita <gosukenator@gmail.com>
2021-11-28 17:40:16 +0900

    Add cert and dns settings

---
+++ terraform/cert.tf
@@ -0,0 +1,22 @@
+resource "aws_acm_certificate" "sample" {
+  domain_name               = aws_route53_record.sample.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "sample_cert_validation" {
+  zone_id = data.aws_route53_zone.sakura_cloud_education_touch_demo_com.zone_id
+  ttl     = 60
+  name    = tolist(aws_acm_certificate.sample.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.sample.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.sample.domain_validation_options)[0].resource_record_value]
+}
+
+resource "aws_acm_certificate_validation" "sample" {
+  certificate_arn         = aws_acm_certificate.sample.arn
+  validation_record_fqdns = [aws_route53_record.sample_cert_validation.fqdn]
+}
---
+++ terraform/dns.tf
@@ -0,0 +1,4 @@
+data "aws_route53_zone" "sakura_cloud_education_touch_demo_com" {
+  name = "sakura-cloud-education-touch-demo.com"
+}
+
