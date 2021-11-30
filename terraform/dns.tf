d611460fe9a9ad9931c9dbac255ccad909b8d56f
Gosuke Miyashita <gosukenator@gmail.com>
2021-11-30 17:04:57 +0900

    Add nginx

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -14,6 +14,18 @@ resource "aws_route53_record" "sample" {
   }
 }
 
+resource "aws_route53_record" "nginx" {
+  zone_id = data.aws_route53_zone.sakura_cloud_education_touch_demo_com.zone_id
+  name    = "nginx.${data.aws_route53_zone.sakura_cloud_education_touch_demo_com.name}"
+  type    = "A"
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_lb.sample.dns_name
+    zone_id                = aws_lb.sample.zone_id
+  }
+}
+
 output "domain_name" {
   value = aws_route53_record.sample.name
 }
